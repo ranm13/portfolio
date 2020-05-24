1e6f278a95458116d955855c201445bfa0d24a60
Ran Marciano <ranm13@gmail.com>
2020-05-24 21:17:12 +0300

    changed js files to jsx

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from './../components/pages/Home';
-import About from './../components/pages/About';
-import Skills from './../components/pages/Skills';
-import Portfolio from './../components/pages/Portfolio';
-import Contact from './../components/pages/Contact';
+import Home from '../components/pages/Home';
+import About from '../components/pages/About';
+import Skills from '../components/pages/Skills';
+import Portfolio from '../components/pages/Portfolio';
+import Contact from '../components/pages/Contact';
 import MenuBar from "../components/menu-bar/MenuBar";
 
 function App() {
