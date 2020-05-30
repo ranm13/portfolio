1ab16a2852badacff1829a467f9d869dc16b082a
Ran Marciano <ranm13@gmail.com>
2020-05-30 23:03:40 +0300

    added the basic skeleton of the contact page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, {Component} from "react";
 
-const Contact = function() {
-  return (
-  <div>
+class Contact extends Component{
+  constructor(){
+    super();
+    this.state={
+      name: "",
+      email: "",
+      subject: "",
+      message: ""
+    }
+  }
 
-  </div>);
-};
+  render(){
+    return (
+      <div className="contact-page-container">
+        <h1>Contact me</h1>
+        <p>Explanation paragraph</p>
+    
+        <form className="contact-info" autoComplete="off" >
+          <input placeholder="Name" name="name" value={this.state.name} type="text" onChange={this.onChange}/>
+          <input placeholder="Email" name="email" value={this.state.email} type="text" onChange={this.onChange}/>
+          <input placeholder="Subject" name="subject" value={this.state.subject} type="text" onChange={this.onChange}/>
+          <textarea placeholder="Message" name="message" value={this.state.message} type="text" onChange={this.onChange}/>
+          <button type="button" onClick={this.send}>Send</button>
+        </form>
+      </div>);
+    };
+
+    onChange = e => {
+      let changedKey = e.target.name;
+      this.setState({ [changedKey] : e.target.value });
+    }
+  
+  send = () => {}
+  }
+
 export default Contact;
